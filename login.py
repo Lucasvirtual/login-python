1f69189531e787688aef17288d3bcd5968abfae5
Lucasvirtual <o_lukinhas@hotmail.com>
2022-11-16 20:08:00 -0300

    Criando tela de login

---
+++ login.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+janela = Tk()
+janela.title("Login")
+janela.geometry("350x250")
+
+labelUsuario = Label("Nome de usuário")
+labelUsuario.place(x=10, y=10)
+
+
+janela.mainloop()
